0455898b151dd6b83b2d4f857030f0c80f16c52f
JinwookKim <jinwook.kim@utexas.com>
2019-11-08 17:27:16 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Getting Started
+
+#### Clone this repo
+```
+git clone https://github.com/JinwookKim/mern-backend-boiler.git
+```
+
+#### Configure .env file
+Create a `.env` file with these credentials in it. It is not included in this project because it is ignored in the `.gitignore` file.
+
+```base
+DB=mongodb+srv://<username>:<password>@<database>-abc12.mongodb.net/test?retryWrites=true&w=majority
+jwtSecret=sl_myJwtSecret
+```
+
+#### Start the server
+```bash
+npm run server
+```
+
+
+
---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(express.json());
+
+// Connect to Mongo
+mongoose.connect(process.env.DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err));
+
+// Use Routes
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
